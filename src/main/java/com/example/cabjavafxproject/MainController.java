f6ca777478c6ec12cf5bd4ad24efe9b7f1408b17
Ryan_from_Home <n11583959@qut.edu.au>
2025-03-18 17:32:50 +1000

    implemented add contact functionality

--- src/main/java/com/example/cabjavafxproject/MainController.java
+++ src/main/java/com/example/cabjavafxproject/MainController.java
@@ -106,4 +106,21 @@ public class MainController {
             syncContacts();
         }
     }
+
+    @FXML
+    private void onAdd() {
+        // Default values for a new contact
+        final String DEFAULT_FIRST_NAME = "New";
+        final String DEFAULT_LAST_NAME = "Contact";
+        final String DEFAULT_EMAIL = "";
+        final String DEFAULT_PHONE = "";
+        Contact newContact = new Contact(DEFAULT_FIRST_NAME, DEFAULT_LAST_NAME, DEFAULT_EMAIL, DEFAULT_PHONE);
+        // Add the new contact to the database
+        contactDAO.addContact(newContact);
+        syncContacts();
+        // Select the new contact in the list view
+        // and focus the first name text field
+        selectContact(newContact);
+        firstNameTextField.requestFocus();
+    }
 }
