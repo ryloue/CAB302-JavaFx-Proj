e594de0a5df450b2dbde9fd6a624c207b7e267a2
Ryan_from_Home <n11583959@qut.edu.au>
2025-03-18 16:59:25 +1000

    created the second page and made next button send you there

--- src/main/java/com/example/cabjavafxproject/HelloController.java
+++ src/main/java/com/example/cabjavafxproject/HelloController.java
@@ -1,10 +1,15 @@
 package com.example.cabjavafxproject;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class HelloController {
     @FXML
@@ -15,6 +20,14 @@ public class HelloController {
     private CheckBox agreeCheckBox;
     @FXML
     private Button nextButton;
+
+    @FXML
+    protected void onNextButtonClick() throws IOException {
+        Stage stage = (Stage) nextButton.getScene().getWindow();
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("main-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), HelloApplication.WIDTH, HelloApplication.HEIGHT);
+        stage.setScene(scene);
+    }
 
     @FXML
     public void initialize() {
