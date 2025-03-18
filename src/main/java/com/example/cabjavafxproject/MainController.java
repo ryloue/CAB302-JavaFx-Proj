a0d77b03dcecd2ec713466c8811fbcb56f69386b
Ryan_from_Home <n11583959@qut.edu.au>
2025-03-18 17:29:09 +1000

    Updated main-controller class with more code and added edit confirm to main-view

--- src/main/java/com/example/cabjavafxproject/MainController.java
+++ src/main/java/com/example/cabjavafxproject/MainController.java
@@ -3,6 +3,8 @@ package com.example.cabjavafxproject;
 import javafx.fxml.FXML;
 import javafx.scene.control.ListCell;
 import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
 
 public class MainController {
     @FXML
@@ -13,12 +15,33 @@ public class MainController {
     }
 
     /**
+     * Programmatically selects a contact in the list view and
+     * updates the text fields with the contact's information.
+     * @param contact The contact to select.
+     */
+    private void selectContact(Contact contact) {
+        contactsListView.getSelectionModel().select(contact);
+        firstNameTextField.setText(contact.getFirstName());
+        lastNameTextField.setText(contact.getLastName());
+        emailTextField.setText(contact.getEmail());
+        phoneTextField.setText(contact.getPhone());
+    }
+
+    /**
      * Renders a cell in the contacts list view by setting the text to the contact's full name.
      * @param contactListView The list view to render the cell for.
      * @return The rendered cell.
      */
     private ListCell<Contact> renderCell(ListView<Contact> contactListView) {
         return new ListCell<>() {
+
+            private void onContactSelected(MouseEvent mouseEvent) {
+                ListCell<Contact> clickedCell = (ListCell<Contact>) mouseEvent.getSource();
+                // Get the selected contact from the list view
+                Contact selectedContact = clickedCell.getItem();
+                if (selectedContact != null) selectContact(selectedContact);
+            }
+
             /**
              * Updates the item in the cell by setting the text to the contact's full name.
              * @param contact The contact to update the cell with.
@@ -49,5 +72,28 @@ public class MainController {
     public void initialize() {
         contactsListView.setCellFactory(this::renderCell);
         contactsListView.getItems().addAll(contactDAO.getAllContacts());
+    }
+
+    @FXML
+    private TextField firstNameTextField;
+    @FXML
+    private TextField lastNameTextField;
+    @FXML
+    private TextField emailTextField;
+    @FXML
+    private TextField phoneTextField;
+
+    @FXML
+    private void onEditConfirm() {
+        // Get the selected contact from the list view
+        Contact selectedContact = contactsListView.getSelectionModel().getSelectedItem();
+        if (selectedContact != null) {
+            selectedContact.setFirstName(firstNameTextField.getText());
+            selectedContact.setLastName(lastNameTextField.getText());
+            selectedContact.setEmail(emailTextField.getText());
+            selectedContact.setPhone(phoneTextField.getText());
+            contactDAO.updateContact(selectedContact);
+            syncContacts();
+        }
     }
 }
